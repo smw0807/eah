59ee6d4b32be850059d69e98275c6fb02366b123
smw0807 <smw0807@gmail.com>
2026-01-05 22:11:58 +0900

    feat: 카테고리 모듈

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,7 +1,111 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Logger,
+  Body,
+  Post,
+  Put,
+  Delete,
+  UseGuards,
+  BadRequestException,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
-
+import { RBAC } from 'src/auth/decorator/rbac';
+import { Role } from 'generated/prisma/enums';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RoleGuard } from 'src/auth/guard/role.guard';
 @Controller('category')
 export class CategoryController {
+  private readonly logger = new Logger(CategoryController.name);
   constructor(private readonly categoryService: CategoryService) {}
+
+  // 카테고리 조회
+  @Get()
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  async getCategories() {
+    return this.categoryService.getCategories();
+  }
+
+  // 최상위 카테고리 조회
+  @Get('get-top-categories')
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  async getTopCategories() {
+    return this.categoryService.getTopCategories();
+  }
+
+  // 카테고리 생성
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  @Post('create')
+  async createCategory(@Body() body: { code: string; name: string }) {
+    const { code, name } = body;
+    if (!name) {
+      throw new BadRequestException('카테고리 이름을 입력해주세요.');
+    }
+    if (!code) {
+      throw new BadRequestException('카테고리 코드를 입력해주세요.');
+    }
+    return this.categoryService.createCategory(code, name);
+  }
+
+  // 카테고리 수정
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  @Put('update')
+  async updateCategory(@Body() body: { id: number; name: string }) {
+    const { id, name } = body;
+    if (!id) {
+      throw new BadRequestException('카테고리 ID를 입력해주세요.');
+    }
+    if (!name) {
+      throw new BadRequestException('카테고리 이름을 입력해주세요.');
+    }
+    return this.categoryService.updateCategory(id, name);
+  }
+
+  // 카테고리 삭제
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  @Delete('delete')
+  async deleteCategory(@Body() body: { id: number }) {
+    const { id } = body;
+    if (!id) {
+      throw new BadRequestException('카테고리 ID를 입력해주세요.');
+    }
+    return this.categoryService.deleteCategory(id);
+  }
+
+  // 카테고리 하위 카테고리 조회
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  @Get('get-sub-categories')
+  async getSubCategories(@Body() body: { parentId: number }) {
+    const { parentId } = body;
+    if (!parentId) {
+      throw new BadRequestException('부모 카테고리 ID를 입력해주세요.');
+    }
+    return this.categoryService.getSubCategories(parentId);
+  }
+
+  // 카테고리 하위 카테고리 생성
+  @RBAC(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
+  @Post('create-sub-category')
+  async createSubCategory(
+    @Body() body: { parentId: number; code: string; name: string },
+  ) {
+    const { parentId, code, name } = body;
+    if (!parentId) {
+      throw new BadRequestException('부모 카테고리 ID를 입력해주세요.');
+    }
+    if (!code) {
+      throw new BadRequestException('카테고리 코드를 입력해주세요.');
+    }
+    if (!name) {
+      throw new BadRequestException('카테고리 이름을 입력해주세요.');
+    }
+    return this.categoryService.createSubCategory(parentId, code, name);
+  }
 }
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Category } from 'generated/prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  private readonly logger = new Logger(CategoryService.name);
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 최상위 카테고리 조회
+  async getTopCategories(): Promise<Category[]> {
+    const topCategories = await this.prisma.category.findMany({
+      where: { parentId: null },
+    });
+    return topCategories;
+  }
+
+  // 카테고리 조회
+  async getCategories(): Promise<Category[]> {
+    const categories = await this.prisma.category.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    });
+    return categories;
+  }
+
+  // 카테고리 생성
+  async createCategory(code: string, name: string): Promise<Category> {
+    const newCategory = await this.prisma.category.create({
+      data: { code, name },
+    });
+    return newCategory;
+  }
+
+  // 카테고리 수정
+  async updateCategory(id: number, name: string): Promise<Category> {
+    const updatedCategory = await this.prisma.category.update({
+      where: { id },
+      data: { name },
+    });
+    return updatedCategory;
+  }
+
+  // 카테고리 삭제
+  async deleteCategory(id: number): Promise<void> {
+    await this.prisma.category.delete({
+      where: { id },
+    });
+  }
+
+  // 카테고리 하위 카테고리 조회
+  async getSubCategories(parentId: number): Promise<Category[]> {
+    const subCategories = await this.prisma.category.findMany({
+      where: { parentId },
+    });
+    return subCategories;
+  }
+
+  // 카테고리 하위 카테고리 생성
+  async createSubCategory(
+    parentId: number,
+    code: string,
+    name: string,
+  ): Promise<Category> {
+    const newSubCategory = await this.prisma.category.create({
+      data: { code, name, parentId },
+    });
+    return newSubCategory;
+  }
+}
