2155682909db56072fb3d4ae0de896fdb3340c64
smw0807 <smw0807@gmail.com>
2026-01-12 22:51:57 +0900

    feat: 프록시 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
