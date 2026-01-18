a1ff784a5525b80b9d4076bb23a29ce22ccb24bb
smw0807 <smw0807@gmail.com>
2026-01-18 10:37:29 +0900

    feat: 경매 상품 등록 화면

--- frontend/src/pages/auction/CreateAuction.tsx
+++ frontend/src/pages/auction/CreateAuction.tsx
@@ -1,3 +1,148 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { useRef, useState } from "react";
+
+type Image = { file: File; previewUrl: string };
+
 export default function CreateAuction() {
-  return <div>CreateAuction</div>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [startPrice, setStartPrice] = useState(0);
+  const [minBidStep, setMinBidStep] = useState(0);
+  const [categoryId, setCategoryId] = useState("");
+  const [subCategoryId, setSubCategoryId] = useState("");
+  const [startAt, setStartAt] = useState(new Date());
+  const [endAt, setEndAt] = useState(new Date());
+  const [imageUrl, setImageUrl] = useState<Image | null>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const file = e.target.files?.[0];
+
+    if (imageUrl) {
+      URL.revokeObjectURL(imageUrl.previewUrl);
+    }
+    setImageUrl({ file, previewUrl: URL.createObjectURL(file) });
+  };
+
+  const handleSubmit = () => {
+    console.log("submit");
+    console.log(title);
+    console.log(description);
+    console.log(startPrice);
+    console.log(minBidStep);
+    console.log(categoryId);
+    console.log(subCategoryId);
+    console.log(startAt);
+    console.log(endAt);
+    console.log(imageUrl);
+  };
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">상품등록</h1>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <Label>상품명</Label>
+          <Input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>상품설명</Label>
+          <Textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>카테고리</Label>
+          <div className="flex gap-2">
+            <Select onValueChange={(value) => setCategoryId(value)}>
+              <SelectTrigger>
+                <SelectValue placeholder="카테고리를 선택해주세요." />
+              </SelectTrigger>
+            </Select>
+            <Select onValueChange={(value) => setSubCategoryId(value)}>
+              <SelectTrigger>
+                <SelectValue placeholder="카테고리를 선택해주세요." />
+              </SelectTrigger>
+            </Select>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>시작가격</Label>
+          <Input
+            type="number"
+            value={startPrice}
+            onChange={(e) => setStartPrice(Number(e.target.value))}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>입찰 단위</Label>
+          <Select onValueChange={(value) => setMinBidStep(Number(value))}>
+            <SelectTrigger>
+              <SelectValue placeholder="입찰 단위를 선택해주세요." />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="100">100원</SelectItem>
+              <SelectItem value="1000">1,000원</SelectItem>
+              <SelectItem value="10000">10,000원</SelectItem>
+              <SelectItem value="100000">100,000원</SelectItem>
+              <SelectItem value="1000000">1,000,000원</SelectItem>
+              <SelectItem value="10000000">10,000,000원</SelectItem>
+              <SelectItem value="100000000">100,000,000원</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <div className="flex flex-col gap-2">
+          <Label>시작일시</Label>
+          <Input
+            type="datetime-local"
+            value={startAt.toISOString().slice(0, 16)}
+            onChange={(e) => setStartAt(new Date(e.target.value))}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label>종료일시</Label>
+          <Input
+            type="datetime-local"
+            value={endAt.toISOString().slice(0, 16)}
+            onChange={(e) => setEndAt(new Date(e.target.value))}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label>상품 이미지</Label>
+        {imageUrl && (
+          <img
+            src={imageUrl.previewUrl}
+            alt="이미지"
+            className="h-100 w-100 object-cover"
+          />
+        )}
+        <Input
+          type="file"
+          ref={imageInputRef}
+          onChange={handleImageChange}
+          accept="image/*"
+        />
+      </div>
+      <Button onClick={handleSubmit}>상품등록</Button>
+    </div>
+  );
 }
